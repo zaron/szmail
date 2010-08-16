ab198fc93471d70b8f8d1526e10fc56d77a7ad20
enrico.grot <enrico.grot@ae271dc9-e7b8-8f74-b2af-ccc6b582b441>
2010-08-16 20:01:53 +0000

    added public folders

---
+++ src/public/modules/net.js
@@ -0,0 +1,107 @@
+//require.define({"net": function(require, exports, module) {
+
+// Empty function.
+var noop = function() {};
+
+function createXMLHttpRequest() {
+	try {
+		return new XMLHttpRequest();
+	} catch (e) {
+	}
+	try {
+		return new ActiveXObject("Msxml2.XMLHTTP");
+	} catch (e) {
+	}
+	alert("XMLHttpRequest not supported");
+	return null;
+}
+
+/**
+ * 
+ * @param url
+ * @param options
+ * @returns
+ */
+var HTTPRequestHandler = function(url, options) {
+
+	options = options || {};
+    options.method = options.method || "GET";
+    options.headers = options.headers || {};
+
+	
+    var _this = this;
+    
+	var writeBuffer = [];
+    
+    var events = {
+    	success : noop,
+    	error   : noop
+    };
+    
+    /**
+     * Registers event callbacks for this request handler.
+     * 
+     * @param eventName
+     * @param callback
+     */
+    this.on = function(eventName, callback) {
+    	events[eventName] = callback;
+	};
+	
+	/**
+	 * Writes a String to the HTTP stream.
+	 * @param s
+	 */
+	this.write = function(s) {
+		writeBuffer.push(s);
+	};
+
+    /**
+     * Interface exposed to the public. 
+     */
+    this.publicInterface = {
+    	//! 
+		on : _this.on,
+		//! 
+		write : _this.write,
+		//! 
+		headers : options.headers
+    };
+	
+    /**
+     * 
+     */
+	this.send = function() {
+		// TODO: Use an engine to write to a HTTP stream (XHR in browsers)
+		log('send called.');
+		var xhr = createXMLHttpRequest();
+		log('xhr created ('+ options.method +','+ url +')');
+		xhr.open(options.method, url, true);
+		log('xhr open.');
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState != 4) { return; } // TODO: Error handling
+			log('xhr received response from \'' + url + '\'');
+			var response = '';
+			if(url == '/'); // bad hack
+			else response = eval('('+xhr.responseText+')');
+			events["success"](response);
+		};
+		xhr.send(null);
+	};
+};
+
+/**
+ * Creates a new HTTP request.
+ * 
+ * @param process
+ */
+exports.createHTTPRequest = function(process) {
+	return {
+		"send" : function(url, options) {
+			var handler = new HTTPRequestHandler(url, options);
+			process(handler.publicInterface);
+			handler.send();
+		}
+	};
+};
+//}});
