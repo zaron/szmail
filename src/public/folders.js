82595c25624a1a8bd8b1ea98c3676e9607a9948a
eric@szlab.de <eric@szlab.de@ae271dc9-e7b8-8f74-b2af-ccc6b582b441>
2010-09-09 20:18:21 +0000

    Stubbing around

---
+++ src/public/folders.js
@@ -0,0 +1,127 @@
+	
+folders = {folders:[{ 
+			"name": "Arbeit", 
+			"global": "Arbeit", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Belege", 
+			"global": "Belege", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Foo", 
+			"global": "Foo", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "INBOX", 
+			"global": "INBOX", 
+			"type": "INBOX", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Junk-E-Mail", 
+			"global": "Junk-E-Mail", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Privat", 
+			"global": "Privat", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Reisen", 
+			"global": "Reisen", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "[Google Mail]", 
+			"global": "[Google Mail]", 
+			"type": "", 
+			"selectable": false, 
+			"isLeaf": false, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Alle Nachrichten", 
+			"global": "[Google Mail]/Alle Nachrichten", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Entw&APw-rfe", 
+			"global": "[Google Mail]/Entw&APw-rfe", 
+			"type": "DRAFTS", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Gesendet", 
+			"global": "[Google Mail]/Gesendet", 
+			"type": "SENT", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Markiert", 
+			"global": "[Google Mail]/Markiert", 
+			"type": "", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Papierkorb", 
+			"global": "[Google Mail]/Papierkorb", 
+			"type": "TRASH", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}, 
+		{
+			"name": "Spam", 
+			"global": "[Google Mail]/Spam", 
+			"type": "SPAM", 
+			"selectable": true, 
+			"isLeaf": true, 
+			"mails": 23, 
+			"unread": 0
+		}]};
